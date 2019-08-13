baae32a3e2d6effc9281b28f928bd6bdfe08eb6e
root <root@builtserver.sepu.com>
2019-08-12 22:31:01 -0300

    commit 4

--- script_teste.sh
+++ script_teste.sh
@@ -3,3 +3,4 @@ echo "Essa saida vem de um script no Jenkins"
 NUM=$(($VALOR1 + $VALOR2))
 echo "A saida do script é: $NUM. A soma de $VALOR1 + $VALOR2"
 echo "linha que permanece"
+echo "nova linha1"
